d8aeba3889fbfa3bc52ba19e4d3ce2572dd9fa8d
Rohan-V <rohankvij@gmail.com>
2022-03-13 18:10:00 -0700

    Added animation to scroll down arrow

--- src/components/ScrollDownArrow.tsx
+++ src/components/ScrollDownArrow.tsx
@@ -2,16 +2,14 @@ import DownArrow from "../../assets/icons/chevron-down.svg";
 import { motion } from "framer-motion";
 
 const ScrollDownArrow = () => {
-  // TODO: add animation!
   return (
     <div>
-      {/*<motion.img
+      <motion.img
         animate={{ y: 20, opacity: [0.1, 0.5] }}
-        transition={{ type: "spring", duration: 3, repeat: Infinity }}
+        transition={{ type: "tween", ease: "easeInOut", duration: 1.5, repeat: Infinity, repeatType: "reverse" }}
         src={DownArrow}
         className="invert w-10 mb-[-20px]"
-  />*/}
-      <img src={DownArrow} className="invert w-10" />
+      />
     </div>
   );
 };
