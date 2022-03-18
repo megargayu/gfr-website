323e1e28be2aa81f6cde335b3a7e647a9b4049dc
Ayush Garg <Garg92484>
2022-03-18 10:40:38 -0700

    started implementing smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import logo from "../assets/logo.png";
-import GFRText from "./components/GFRText";
-
-import ScrollDownArrow from "./components/ScrollDownArrow";
+import React, { useRef } from "react";
+import Panel1 from "./components/panels/Panel1";
 
 const App: React.FC = () => {
+  const panels = [
+    useRef<HTMLDivElement | null>(null),
+    useRef<HTMLDivElement | null>(null),
+  ];
+
+  const scroll = (panel: number) => {
+    panels[panel]?.current?.scrollIntoView();
+  };
+
   return (
     <>
-      <div className="w-full h-screen bg-dark font-body b-0">
-        <div className="flex flex-col justify-center items-center h-full">
-          <div
-            className="flex flex-col justify-center items-center h-[calc(100%_-_2.5rem_-_100px)]"
-          >
-            <img
-              className="w-80 h-80 mb-1 saturate-200 drop-shadow-[0_0_20px_rgba(255,255,255,0.3)] justify-self-center"
-              src={logo}
-            />
+      <Panel1 ref={panels[0]} scroll={scroll} />
 
-            <GFRText />
-          </div>
-
-          <button>
-            <ScrollDownArrow />
-          </button>
-        </div>
-      </div>
-
-      <div className="w-full h-screen bg-blue-900 font-body b-0 p-10">
+      <div
+        className="w-full h-screen bg-blue-900 font-body b-0 p-10"
+        ref={panels[1]}
+      >
         <h1 className="text-6xl font-semibold text-white text-center">
           Panel 2
         </h1>
