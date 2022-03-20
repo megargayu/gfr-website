efcf3fc9f3efa0263af0177c38b519c606c63afc
megargayu <megargayu@gmail.com>
2022-03-20 12:48:24 -0700

    added smooth scrolling (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useRef } from "react";
+import checkIfScrolling from "./utils/checkIfScrolling";
 
 import Panel from "./components/panels/Panel";
 import TopPanel from "./components/panels/TopPanel";
 import TeamPanel from "./components/panels/TeamPanel";
 import Panel3 from "./components/panels/Panel3";
 
-// TODO: smooth scroll snap
 const App = () => {
   const panels = [TopPanel, TeamPanel, Panel3];
   const refs = Array.from({ length: panels.length }, () =>
@@ -16,8 +16,20 @@ const App = () => {
     refs[panel]?.current?.scrollIntoView();
   };
 
+  // TODO: handle different panels properly
+  const smoothScroll = (e: WheelEvent) => {
+    e.preventDefault();
+
+    if (!checkIfScrolling()) {
+      if (e.deltaY > 0) scrollToPanel(1);
+      else if (e.deltaY < 0) scrollToPanel(0);
+    }
+  };
+
+  document.addEventListener("wheel", smoothScroll, { passive: false });
+
   return (
-    <div className="">
+    <div>
       {panels.map((panel, i) => {
         return (
           <Panel
---
+++ src/utils/checkIfScrolling.tsx
@@ -0,0 +1,14 @@
+var scrollTimeout: NodeJS.Timeout;
+let isScrolling = false;
+
+addEventListener("scroll", () => {
+  isScrolling = true;
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(() => (isScrolling = false), 100);
+});
+
+const checkIfScrolling = () => {
+  return isScrolling;
+}
+
+export default checkIfScrolling;
