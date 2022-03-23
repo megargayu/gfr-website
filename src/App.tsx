9d786e6507927e1eb13ba35226e1abbea84d6588
megargayu <megargayu@gmail.com>
2022-03-22 18:24:21 -0700

    use spring transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React, { useRef } from "react";
-import checkIfScrolling from "./utils/checkIfScrolling";
 
 import Panel from "./components/panels/Panel";
 import TopPanel from "./components/panels/TopPanel";
 import TeamPanel from "./components/panels/TeamPanel";
 import Panel3 from "./components/panels/Panel3";
+import { useSpring } from "framer-motion";
 
 const App = () => {
+  const scrollSpring = useSpring(window.scrollY, { duration: 500 });
+  scrollSpring.onChange((v) => window.scrollTo({ top: v }));
+
   const panels = [TopPanel, TeamPanel, Panel3];
   const refs = Array.from({ length: panels.length }, () =>
     useRef<HTMLDivElement | null>(null)
   );
 
-  const scrollToPanel = (panel: number) => {
-    if (checkIfScrolling()) return;
-    refs[panel].current?.scrollIntoView();
+  const scrollToPanel = (panelNum: number) => {
+    if (scrollSpring.getVelocity() != 0) return;
+
+    const panel = refs[panelNum].current;
+    if (panel == null) return;
+
+    scrollSpring.set(window.scrollY, false);
+    scrollSpring.set(panel.offsetTop);
   };
 
   // TODO: handle different panels properly
@@ -48,6 +56,7 @@ const App = () => {
   document.addEventListener("wheel", smoothScroll, { passive: false });
   document.addEventListener("keydown", smoothKeys, { passive: false });
   // TODO: handle touch
+  // TODO: fix wheel & key events; too many can cause it to glitch out (wheel events are delayed, and key events are weird)
 
   return (
     <div>
